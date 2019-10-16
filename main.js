6d9114e2a1a783661af158d18fc85de2b82085a8
Nidhee Kamble <38733826+nidheekamble@users.noreply.github.com>
2019-10-16 05:14:39 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+var endPoint = "https://www.jsonstore.io/b198970ae4af630ad693075a720ac4b703881b951d47af7b1e3ba686de367116";
 
+function getRandomString() {
+  var _randomString = Math.random.toString(32).substring(2.5) + Math.random.toString(32).substring(2.5);
+  return _randomString;
+}
