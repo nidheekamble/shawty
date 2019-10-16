12c6c2dd454a940b1922ad00a4630107df770487
Nidhee Kamble <38733826+nidheekamble@users.noreply.github.com>
2019-10-16 05:48:24 +0000

    Base extraction functions added

--- main.js
+++ main.js
@@ -4,3 +4,39 @@ function getRandomString() {
   var _randomString = Math.random.toString(32).substring(2.5) + Math.random.toString(32).substring(2.5);
   return _randomString;
 }
+
+function getURL() {
+  var URLinput = document.getElementById("URLinput").value;
+  var hasProtocol = URLinput.startsWith("https://") || URLinput.startsWith("http://") || URLinput.startsWith("ftp://");
+  if(!hasProtocol)
+  {
+    var correctedURL = "http://"+URLinput;
+    return correctedURL;
+  }
+  else
+    return URLinput;
+}
+
+function generateHash() {
+  if (window.location.hash = "")
+  {
+    window.location.hash = getrandom();
+  }
+}
+
+function sendRequest(url) {
+  this.url = url;
+  $.ajax({
+    'url': endPoint+"/"+window.location.hash.substr(1),
+    'type': 'POST',
+    'data': JSON.stringify(this.url),
+    'dataType': 'json',
+    ‘contentType’: ‘application/json; charset=utf-8’
+  });
+}
+
+function shortenURL() {
+  var URLinput = getURL();
+  generateHash();
+  sendRequest(URLinput);
+}
